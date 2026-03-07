a2793fa935aadf6a48868088447c349901819dc5
repalleramr <repalleramr@gmail.com>
2026-03-08 02:48:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('#panel-yaksha .num-btn').forEach(b=>{
+b.onclick=()=>alert('Yaksha '+b.innerText);
+});
+
+document.querySelectorAll('#panel-kinnara .num-btn').forEach(b=>{
+b.onclick=()=>alert('Kinnara '+b.innerText);
+});
