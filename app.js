3e4062782bc49db88f03e3eb9ea2882da6e0e055
repalleramr <repalleramr@gmail.com>
2026-03-08 02:45:48 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll('#panel-yaksha .num-btn').forEach(b=>{
-b.onclick=()=>alert('Yaksha '+b.innerText);
-});
-
-document.querySelectorAll('#panel-kinnara .num-btn').forEach(b=>{
-b.onclick=()=>alert('Kinnara '+b.innerText);
-});
